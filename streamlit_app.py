f4a7f8541bc8352b00f44735d959a726cfbd9ef3
athayahisyam <hisyamathaya@unida.gontor.ac.id>
2023-05-24 18:36:03 +0700

    New Updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.write('Hello World')
+st.set_page_config(page_title="Hisyam Athaya, the Data Scientist", page_icon=":smiley:", layout="wide")
+
+st.subheader("Hi, my name is Hisyam, from Indonesia, :wave:")
+st.title("I am a Data Scientist :sunglasses:")
+st.write("I am starting Data Scientist. I have a strong background in Machine Learning, Deep Learning, and Natural Language Processing. I have a passion for learning and sharing my knowledge with others as publicly as possible. I have a strong interest in Data Science, Machine Learning, and Deep Learning. I am also a fast learner and a team player who is eager to learn from others and share my knowledge with them. I am a self-motivated person who is always looking for new challenges and opportunities to grow professionally and personally.")
